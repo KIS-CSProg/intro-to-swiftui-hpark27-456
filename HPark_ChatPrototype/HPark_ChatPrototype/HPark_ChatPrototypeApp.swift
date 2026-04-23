8f15c82978a7c0dc5317b802fd9838e330d7801f
Hyerin <hpark27@student.kis.or.kr>
2026-04-23 09:16:24 +0900

    Create Xcode ChatPrototype

---
+++ HPark_ChatPrototype/HPark_ChatPrototype/HPark_ChatPrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HPark_ChatPrototypeApp.swift
+//  HPark_ChatPrototype
+//
+//  Created by Hyerin Park on 4/23/26.
+//
+
+import SwiftUI
+
+@main
+struct HPark_ChatPrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
